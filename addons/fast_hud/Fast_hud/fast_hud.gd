691b6aef18e613ff0995fd442360901997e81fa8
Théo Matrod <75441296+C4RBON73@users.noreply.github.com>
2025-12-14 18:39:09 +0100

    Ajout du HUD dans le load du plugin

--- addons/fast_hud/Fast_hud/fast_hud.gd
+++ addons/fast_hud/Fast_hud/fast_hud.gd
@@ -14,10 +14,10 @@ func _disable_plugin() -> void:
 func _enter_tree():
 	# Initialization of the plugin goes here.
 	# Add the new type with a name, a parent type, a script and an icon.
-	add_custom_type("TestButton", "Button", preload("res://addons/fast_hud/Fast_hud/test_button.gd"), preload("res://icon.svg"))
+	add_custom_type("HUD", "Container", preload("res://addons/fast_hud/Fast_hud/container.gd"), preload("res://icon.svg"))
 
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
 	# Always remember to remove it from the engine when deactivated.
-	remove_custom_type("MyButton")
+	remove_custom_type("HUD")
